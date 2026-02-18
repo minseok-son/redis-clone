6a4e9a0011512346661b571e4c85bac78933d6f4
Minseok Son <mseok.son@gmail.com>
2026-02-18 13:34:56 -0600

    feat: added support for setting expiration on key

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,7 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/minseok-son/redis-clone/internal/resp"
 	"github.com/minseok-son/redis-clone/internal/storage"
@@ -14,6 +16,7 @@ func main() {
 	listener, _ := net.Listen("tcp", ":6379")
 	fmt.Println("Server is listening on port 6379...")
 	db := storage.NewDB()
+	db.StartJanitor(time.Second)
 
 	for {
 		conn, _ := listener.Accept()
@@ -42,7 +45,7 @@ func handleCommand(parts []string, c net.Conn, db *storage.DB) {
 
 	switch strings.ToUpper(parts[0]) {
 	case "GET":
-		if len(parts) < 2 {
+		if len(parts) != 2 {
 			c.Write([]byte("-ERR wrong number of arguments for 'get' command\r\n"))
 			return
 		}
@@ -53,15 +56,26 @@ func handleCommand(parts []string, c net.Conn, db *storage.DB) {
 		}
 		c.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(val), val)))
 	case "SET":
-		if len(parts) < 3 {
+		if len(parts) == 3 {
+			db.Set(parts[1], parts[2], 0)
+			c.Write([]byte("+OK\r\n"))
+		} else if len(parts) == 5 && strings.ToUpper(parts[3]) == "EX" {
+			s, err := strconv.ParseInt(parts[4], 10, 64)
+			if err != nil {
+				c.Write([]byte("-ERR value is not an integer or out of range\r\n"))
+				return
+			}
+
+			expiresAt := time.Now().Add(time.Duration(s) * time.Second).UnixNano()
+
+			db.Set(parts[1], parts[2], expiresAt)
+			c.Write([]byte("+OK\r\n"))
+		} else {
 			c.Write([]byte("-ERR wrong number of arguments for 'set' command\r\n"))
 			return
 		}
-		// parts[0] is "SET", parts[1] is key, parts[2] is value
-		db.Set(parts[1], parts[2]) 
-		c.Write([]byte("+OK\r\n")) // Redis returns +OK after a successful SET
 	case "DEL":
-		if len(parts) < 2 {
+		if len(parts) != 2 {
 			c.Write([]byte("-ERR wrong number of arguments for 'del' command\r\n"))
 			return
 		}
--- internal/storage/store.go
+++ internal/storage/store.go
@@ -2,30 +2,39 @@ package storage
 
 import (
 	"sync"
+	"time"
 )
+
+type Item struct {
+	Value string
+	ExpiresAt int64
+}
 
 type DB struct {
 	mu sync.RWMutex
-	data map[string]string
+	data map[string]Item
 }
 
 func NewDB() *DB {
 	return &DB{
-		data: make(map[string]string),
+		data: make(map[string]Item),
 	}
 }
 
-func (db *DB) Set(key string, value string) {
+func (db *DB) Set(key string, value string, timestamp int64) {
 	db.mu.Lock()
 	defer db.mu.Unlock()
-	db.data[key] = value
+	db.data[key] = Item {
+		Value: value,
+		ExpiresAt: timestamp,
+	}
 }
 
 func (db *DB) Get(key string) (string, bool) {
 	db.mu.RLock()
 	defer db.mu.RUnlock()
-	val, ok := db.data[key]
-	return val, ok
+	item, ok := db.data[key]
+	return item.Value, ok
 }
 
 func (db *DB) Del(key string) int {
@@ -37,4 +46,28 @@ func (db *DB) Del(key string) int {
 		return 1
 	}
 	return 0
+}
+
+func (db *DB) StartJanitor(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+
+	go func() {
+		for {
+			<-ticker.C
+
+			db.DeleteExpired()
+		}
+	}()
+}
+
+func (db *DB) DeleteExpired() {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	now := time.Now().UnixNano()
+	for key, item := range db.data {
+		if item.ExpiresAt > 0 && now > item.ExpiresAt {
+			delete(db.data, key)
+		}
+	}
 }
